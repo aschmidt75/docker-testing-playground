e04c502f0cf927bdc52e42397503520d43921d7b
Andreas Schmidt <andreas.schmidt@cassini.de>
2014-07-16 15:22:48 +0200

    added spec for docker installation

--- spec.d/spec/localhost/docker_spec.rb
+++ spec.d/spec/localhost/docker_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe 'It should have docker installed' do
+	describe package 'lxc-docker' do
+		it { should be_installed }
+	end
+
+	describe group 'docker' do
+		it { should exist }
+	end
+
+	describe file '/var/run/docker.sock' do
+		it { should be_socket }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'docker' }
+	end
+
 end
