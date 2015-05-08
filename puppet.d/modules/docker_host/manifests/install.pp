a6f1d56d1d0ccb9b3a4446f0e0cdcd50f3c20c79
Christian Neudert <christian.neudert86@gmail.com>
2015-05-08 20:54:41 +0200

    Update to current docker version 1.6.1.

--- puppet.d/modules/docker_host/manifests/install.pp
+++ puppet.d/modules/docker_host/manifests/install.pp
@@ -2,7 +2,7 @@ include 'docker'
 
 class docker_host::install {
   class { 'docker':
-    version       => '1.1.1',
+    version       => '1.6.1',
     manage_kernel => false,
     tcp_bind      => 'tcp://127.0.0.1:2375',
     socket_bind   => 'unix:///var/run/docker.sock',
